bc90fd18f5ecf0cfb78d360106f3d7ffda73d772
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-08-06 22:55:11 -0300

    adicionando enveto de tocar

--- script/script.js
+++ script/script.js
@@ -24,3 +24,27 @@ function voltar(){
     cartoes[cartaoatual].setAttribute('id', 'selecionado')
 }
 }
+function tavancar(){
+    if(cartaoatual > 22){
+        window.alert('Fim Da Lista!') 
+        cartaoatual = 23
+    } else {
+    var cartoes = document.querySelectorAll('.Bosses')
+    console.log(cartoes[cartaoatual])
+    cartaoatual += 1
+    cartoes[cartaoatual-1].removeAttribute('id')
+    cartoes[cartaoatual].setAttribute('id', 'selecionado')
+} 
+}
+function tvoltar(){
+    if(cartaoatual < 1){
+        window.alert('Começo Da Lista!') 
+        cartaoatual = 0
+    } else{
+    var cartoes = document.querySelectorAll('.Bosses')
+    console.log(cartoes[cartaoatual])
+    cartaoatual -= 1
+    cartoes[cartaoatual + 1].removeAttribute('id')
+    cartoes[cartaoatual].setAttribute('id', 'selecionado')
+}
+}
