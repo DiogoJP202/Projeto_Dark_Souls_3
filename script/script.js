a69042953344b882d5cd2391f23e29859adcdb54
Diogo Antonny <102389309+DiogoJP202@users.noreply.github.com>
2022-08-08 14:40:41 -0300

    script para touch

--- script/script.js
+++ script/script.js
@@ -24,19 +24,7 @@ function voltar(){
     cartoes[cartaoatual].setAttribute('id', 'selecionado')
 }
 }
-function tavancar(){
-    if(cartaoatual > 23){
-        window.alert('Fim Da Lista!') 
-        cartaoatual = 23
-    } else {
-    var cartoes = document.querySelectorAll('.Bosses')
-    console.log(cartoes[cartaoatual])
-    cartaoatual += 1
-    cartoes[cartaoatual-1].removeAttribute('id')
-    cartoes[cartaoatual].setAttribute('id', 'selecionado')
-} 
-}
-function tvoltar(){
+let botãoV = document.getElementById('voltar').addEventListener('touchstart', () => { 
     if(cartaoatual < 1){
         window.alert('Começo Da Lista!') 
         cartaoatual = 0
@@ -47,4 +35,16 @@ function tvoltar(){
     cartoes[cartaoatual + 1].removeAttribute('id')
     cartoes[cartaoatual].setAttribute('id', 'selecionado')
 }
+})
+let botãoP = document.getElementById('proximo').addEventListener('touchstart', () => {
+if(cartaoatual > 23){
+        window.alert('Fim Da Lista!') 
+        cartaoatual = 23
+    } else {
+    var cartoes = document.querySelectorAll('.Bosses')
+    console.log(cartoes[cartaoatual])
+    cartaoatual += 1
+    cartoes[cartaoatual-1].removeAttribute('id')
+    cartoes[cartaoatual].setAttribute('id', 'selecionado')
+} 
 }
